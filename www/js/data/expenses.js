775c8db3306cde5e948f658ce08a29995e562fdd
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2017-01-12 13:24:41 +0000

    Add files via upload

---
+++ www/js/data/expenses.js
@@ -0,0 +1,33 @@
+/*
+var expense = new Object (description,date,amt_in,amt_out) {
+    this.description = description;
+    this.date = date;
+    this.amt_in = amt_in;
+    this.amt_out = amt_out;
+}
+*/
+
+
+
+
+
+var expenses = [
+    {
+        description: "first",
+        date: new Date(),
+        amt_in: 500,
+        amt_out:30
+    },
+    {
+        description: "first",
+        date: new Date(),
+        amt_in: 500,
+        amt_out:30
+    },
+    {
+        description: "first",
+        date: new Date(),
+        amt_in: 500,
+        amt_out:30
+    }
+]
